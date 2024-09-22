2d56713c7d96e9206870b8d430de9fb4b9a2b447
Akemi Marleny <akemi.marleny@gmail.com>
2024-09-22 15:30:46 +0200

    chore: initial commit

---
+++ src/test/java/co/idesoft/jacal_services/JacalServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.idesoft.jacal_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacalServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
