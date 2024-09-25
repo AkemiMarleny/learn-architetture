51028ef84dfd73f379730938f1161d755b808e66
Akemi Marleny <akemi.marleny@gmail.com>
2024-09-25 21:42:11 +0200

    feat: get dettaglio cassa

--- src/main/java/co/idesoft/jacalservices/prodotto/mvcservices/controllers/CassaController.java
+++ src/main/java/co/idesoft/jacalservices/prodotto/mvcservices/controllers/CassaController.java
@@ -5,14 +5,18 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import co.idesoft.jacalservices.prodotto.mvcservices.controllers.dto.CassaCreatoDto;
+import co.idesoft.jacalservices.prodotto.mvcservices.controllers.dto.CassaDettaglioDto;
 import co.idesoft.jacalservices.prodotto.mvcservices.controllers.dto.CassaItemDto;
 import co.idesoft.jacalservices.prodotto.mvcservices.controllers.dto.CreareCassaDto;
+import co.idesoft.jacalservices.prodotto.mvcservices.entities.Cassa;
+import co.idesoft.jacalservices.prodotto.mvcservices.exceptions.RecordNotFoundException;
 import co.idesoft.jacalservices.prodotto.mvcservices.services.CassaService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -37,6 +41,19 @@ public class CassaController {
         Page<CassaItemDto> cassePage = cassaService.findAll(pageable)
                 .map(CassaItemDto::fromEntity);
         return ResponseEntity.ok(cassePage);
+    }
+
+    @GetMapping("{cassaId}")
+    public ResponseEntity<CassaDettaglioDto> getDettaglioCassa(@PathVariable Long cassaId) {
+
+        try {
+            Cassa cassaEnt = cassaService.findDettaglio(cassaId);
+            CassaDettaglioDto cassaDettaglioDto = CassaDettaglioDto.fromEntity(cassaEnt);
+            return new ResponseEntity<>(cassaDettaglioDto, HttpStatus.OK);
+        } catch (RecordNotFoundException e) {
+
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
 }
--- src/main/java/co/idesoft/jacalservices/prodotto/mvcservices/services/CassaService.java
+++ src/main/java/co/idesoft/jacalservices/prodotto/mvcservices/services/CassaService.java
@@ -7,6 +7,7 @@ import org.springframework.validation.annotation.Validated;
 
 import co.idesoft.jacalservices.prodotto.mvcservices.controllers.dto.CreareCassaDto;
 import co.idesoft.jacalservices.prodotto.mvcservices.entities.Cassa;
+import co.idesoft.jacalservices.prodotto.mvcservices.exceptions.RecordNotFoundException;
 import co.idesoft.jacalservices.prodotto.mvcservices.repositories.CassaRepository;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -24,5 +25,9 @@ public class CassaService {
 
     public Page<Cassa> findAll(Pageable pageable) {
         return cassaRepository.findAll(pageable);
+    }
+
+    public Cassa findDettaglio(Long cassaId) throws RecordNotFoundException {
+        return cassaRepository.findById(cassaId).orElseThrow(RecordNotFoundException::new);
     }
 }
