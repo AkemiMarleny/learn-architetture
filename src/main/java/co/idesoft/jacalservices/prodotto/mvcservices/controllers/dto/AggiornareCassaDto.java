e71a52470c4121c4fa0ca558aa1af302dcd4ba05
Akemi Marleny <akemi.marleny@gmail.com>
2024-09-27 16:08:11 +0200

    fix: sono state aggiunte validazioni al record

--- src/main/java/co/idesoft/jacalservices/prodotto/mvcservices/controllers/dto/AggiornareCassaDto.java
+++ src/main/java/co/idesoft/jacalservices/prodotto/mvcservices/controllers/dto/AggiornareCassaDto.java
@@ -1,6 +1,10 @@
 package co.idesoft.jacalservices.prodotto.mvcservices.controllers.dto;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.NotBlank;
+
 public record AggiornareCassaDto(
-        String nome,
+        @NotBlank @Length(min = 3) String nome,
         String descrizione) {
 }
