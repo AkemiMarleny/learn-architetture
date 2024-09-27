daada6361c0d04e113f6197191326c52fecca848
Akemi Marleny <akemi.marleny@gmail.com>
2024-09-27 16:10:38 +0200

    fix: sono state aggiunte validazioni al record

--- src/main/java/co/idesoft/jacalservices/prodotto/mvcservices/controllers/dto/AggiornareCategoriaDto.java
+++ src/main/java/co/idesoft/jacalservices/prodotto/mvcservices/controllers/dto/AggiornareCategoriaDto.java
@@ -1,7 +1,11 @@
 package co.idesoft.jacalservices.prodotto.mvcservices.controllers.dto;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.NotBlank;
+
 public record AggiornareCategoriaDto(
-        String nome,
+        @NotBlank @Length(min = 3) String nome,
         String descrizione) {
 
 }
