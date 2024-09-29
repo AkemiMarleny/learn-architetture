cd86970900aa89adeb1c7be559010704406bde10
Akemi Marleny <akemi.marleny@gmail.com>
2024-09-29 19:58:30 +0200

    fix: sono state aggiunte validazioni al record

--- src/main/java/co/idesoft/architetture/mvc/controllers/dto/AggiornareProdottoDto.java
+++ src/main/java/co/idesoft/architetture/mvc/controllers/dto/AggiornareProdottoDto.java
@@ -1,8 +1,11 @@
 package co.idesoft.architetture.mvc.controllers.dto;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.NotBlank;
+
 public record AggiornareProdottoDto(
-        Long id,
-        String nome,
+        @NotBlank @Length(min=3) String nome,
         String descrizione) {
 
 }
