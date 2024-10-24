104ad4ca6919757e544664ef293d4671dd1868d7
Akemi Marleny <akemi.marleny@gmail.com>
2024-10-24 20:59:05 +0200

    feat(prodotto): aggiunto unita di misura su lista

--- src/main/java/co/idesoft/architetture/mvc/controllers/dto/ProdottoItemDto.java
+++ src/main/java/co/idesoft/architetture/mvc/controllers/dto/ProdottoItemDto.java
@@ -5,12 +5,16 @@ import co.idesoft.architetture.mvc.entities.Prodotto;
 public record ProdottoItemDto(
         Long prodottoId,
         String nome,
+        Long unitaMisuraId,
+        String unitaMisuraNome,
         String descrizione) {
 
     public static ProdottoItemDto fromEntity(Prodotto prodotto) {
         return new ProdottoItemDto(
                 prodotto.getProdottoId(),
                 prodotto.getNome(),
+                prodotto.getUnitaMisuraId(),
+                prodotto.getUnitaMisura().getNome(),
                 prodotto.getDescrizione());
     }
 }
