3331cbb8e241d1763e5ac4e336c3af47ea9ec8f1
AkemiMarleny <akemi.marleny@gmail.com>
2024-10-24 21:17:10 +0200

    feat(prodotto): aggiunto unita di misura su dettaglio (#1)

--- src/main/java/co/idesoft/architetture/mvc/controllers/dto/ProdottoDettaglioDto.java
+++ src/main/java/co/idesoft/architetture/mvc/controllers/dto/ProdottoDettaglioDto.java
@@ -5,12 +5,16 @@ import co.idesoft.architetture.mvc.entities.Prodotto;
 public record ProdottoDettaglioDto(
         Long prodottoId,
         String nome,
+        Long unitaMisuraId,
+        String unitaMisuraNome,
         String descrizione) {
 
     public static ProdottoDettaglioDto fromEntity(Prodotto prodotto) {
         return new ProdottoDettaglioDto(
                 prodotto.getProdottoId(),
                 prodotto.getNome(),
+                prodotto.getUnitaMisuraId(),
+                prodotto.getUnitaMisura().getNome(),
                 prodotto.getDescrizione());
     }
 
