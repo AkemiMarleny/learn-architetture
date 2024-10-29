d3fbf33bc831df2967aa4c3fab693bc8e171e341
Akemi Marleny <akemi.marleny@gmail.com>
2024-10-29 18:01:13 +0100

    feat(warehouse): refactor del cancellare warehouse

--- src/main/java/co/idesoft/architetture/mvc/repositories/ProdottoDisponibilitaRepository.java
+++ src/main/java/co/idesoft/architetture/mvc/repositories/ProdottoDisponibilitaRepository.java
@@ -8,4 +8,6 @@ public interface ProdottoDisponibilitaRepository extends JpaRepository<ProdottoD
 
     @Query("SELECT SUM(p.quantita) FROM ProdottoDisponibilita p WHERE p.prodottoId= ?1")
     Long sumQuantitaByProdottoId(Long prodottoId);
+
+    Long countByWarehouseId(Long warehouseId);
 }
