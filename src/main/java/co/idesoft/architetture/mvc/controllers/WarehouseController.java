e877a12d5182dcdbb3f9587837295bc76e21c78c
AkemiMarleny <akemi.marleny@gmail.com>
2024-10-29 18:05:03 +0100

    feat(warehouse): refactor del cancellare warehouse (#6)

--- src/main/java/co/idesoft/architetture/mvc/controllers/WarehouseController.java
+++ src/main/java/co/idesoft/architetture/mvc/controllers/WarehouseController.java
@@ -1,7 +1,8 @@
 package co.idesoft.architetture.mvc.controllers;
 
+import co.idesoft.architetture.mvc.entities.Warehouse;
+import co.idesoft.architetture.mvc.repositories.ProdottoDisponibilitaRepository;
 import co.idesoft.architetture.mvc.repositories.WarehouseRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RequestMapping("api/warehouses")
 @RestController
 @RequiredArgsConstructor
@@ -18,17 +21,33 @@ import org.springframework.web.bind.annotation.RestController;
 public class WarehouseController {
 
     private final WarehouseRepository warehouseRepository;
+    private final ProdottoDisponibilitaRepository prodottoDisponibilitaRepository;
 
-    @Transactional
+    //@Transactional
     @DeleteMapping("{warehouseId}")
     public ResponseEntity<Void> cancellaWarehouse(@PathVariable Long warehouseId) {
 
-        int risultato = warehouseRepository.deleteByWareHouseId(warehouseId);
-        log.info("risultato: {}", risultato);
+        Optional<Warehouse> warehouseOpt = warehouseRepository.findById(warehouseId);
 
-        if (risultato == 1) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        if (warehouseOpt.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        // no permite eliminar (se usa el metodo countBy...) porque warehouseId tiene una relacion con otra tabla
+        long totaleWarehouseId = prodottoDisponibilitaRepository.countByWarehouseId(warehouseId);
+
+        if (totaleWarehouseId > 0) {
+            return new ResponseEntity<>(HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+
+        warehouseRepository.deleteById(warehouseId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+        //      int risultato = warehouseRepository.deleteByWareHouseId(warehouseId);
+//        log.info("risultato: {}", risultato);
+//
+//        if (risultato == 1) {
+//            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+//        }
     }
 }
